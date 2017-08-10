5700f60ded369abcabba2ff81f0243f8bba4a5ab
Lisa M. Copeland <lisacope@msn.com>
2017-08-10 11:05:38 -0700

    Added model for user-deck

---
+++ assets/app/models/user-deck.model.ts
@@ -0,0 +1,9 @@
+export class UserDeck {
+
+	constructor(
+		public lastPlayed: date,
+		public progressBar: string,
+		public deckId: string,
+		public userId: string,
+		public userDeckId: string
+}
