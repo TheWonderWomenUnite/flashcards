674346f486dae1904c5dff1a305dda35fcaf06e5
Diane Zevenbergen <dianezev@comcast.net>
2017-08-18 16:53:52 -0700

    restore route stuff, mostly

--- assets/app/app-routing.module.ts
+++ assets/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { flashcard_routes } from './play-flash-cards/flashcard.routes';
 import { makecard_routes } from './make-flash-cards/makecard.routes';
 
 const appRoutes: Routes = [
+	// DMZ I commented this out - couldn't figure out another way to prevent auth comp from displaying at start
 	// { path: '', redirectTo: '/auth', pathMatch: 'full' },
 	{ path: 'auth', component: AuthenticationComponent, children: auth_routes },
 	{ path: 'makeflashcards', component: MakeFlashCardsComponent, children: makecard_routes },
