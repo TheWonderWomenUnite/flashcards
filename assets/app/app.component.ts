5887c1f76ede15f65bdd66a85c5c573055ef816d
Diane Zevenbergen <dianezev@comcast.net>
2017-09-20 10:59:17 -0700

    work on sidebar

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -9,12 +9,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   loginPrompt = 'LOG IN/SIGN UP';
   title = 'app works!';
+  hideSidebar = true;
 
-  // TBD/ Q for Lisa: trying to get event emitter from signin to call this
+  // TBD - Q for Lisa: trying to get event emitter from signin to call this
   onLoggedIn(loginData: {status: boolean}) {
     console.log('app.component.ts called onLoggedIn')
     this.loginPrompt = loginData.status ? 'LOG OUT' : 'LOG IN/SIGN UP';
   }
+  toggleSidebar() {
+    console.log('this.hideSidebar is ' + this.hideSidebar);
+    this.hideSidebar = !this.hideSidebar;
+  }
 
+  // DMZ added these in place of routerLinks in order to handle toggling of sidebar
+  onPlayCards() {
+    this.hideSidebar = true;
+    this.router.navigate(['./playflashcards/']);
+  }
+  onMakeCards() {
+    this.hideSidebar = true;
+    this.router.navigate(['./makeflashcards/']);
+  }
+  onLogin() {
 
+  }
 }
