2b79a420f40fec90134666535852d991f7d761c4
Diane Zevenbergen <dianezev@comcast.net>
2017-09-27 15:49:34 -0700

    play-list sort option

--- assets/app/play-flash-cards/deck-list.component.ts
+++ assets/app/play-flash-cards/deck-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params, Router } from '@angular/router';
 
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
+
 import { Deck } from "../models/deck.model";
 import { DeckService } from "../shared/deck.service";
 // import { DeckDetailComponent } from './deck.component';
@@ -21,12 +23,31 @@ export class DeckListComponent implements OnInit {
     decks: Deck[];
     userId: string;
 
+    // Attributes for the "sort by" drop down
+    optionChoice: number[];
+    sortOptions: IMultiSelectOption[];
+    sortTexts: IMultiSelectTexts = { defaultTitle: 'Sort By' };
+
+
+    // These settings limit dropdown to one choice (for both drop downs)
+    dropSettings: IMultiSelectSettings = {
+        selectionLimit: 1,
+        autoUnselect: true
+        };
+
+
+
     constructor(private route: ActivatedRoute,
                 private deckService: DeckService,
                 private utilsService: UtilsService) {
     }
 
     ngOnInit() {
+        this.sortOptions = [
+            { id: 1, name: 'Category' },
+            { id: 2, name: 'Last Played' },
+            { id: 3, name: 'Favorites' },
+        ];
 
         // Get the current userId from local storage 
         // this.userId = localStorage.getItem('UserId');
@@ -47,15 +68,27 @@ export class DeckListComponent implements OnInit {
             });            
     }
 
-    onSortBy(sortOrder:string) {
-        console.log("Sort by "+sortOrder);
-    }
-
     onGoBack() {
         // This component should have a back button that takes you 
         // back to a general welcome screen
     }
 
+    // onSortBy(sortOrder:string) {
+    onSortBy() {
+        console.log("Sort decks by "+this.optionChoice);
+        switch(this.optionChoice[0]) {
+            case 1:
+                // Sort decks by category
+                break;
+            case 2:
+                // Sort decks by last played
+                break;
+            case 3:
+                // Sort decks by favorites
+                break;
+        }
+    }
+    
     /*
     This was temporarily here to test the deck service
     To add it back in, add an "add deck" button to the template
